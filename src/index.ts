43db811a03b7785c9365c5557f8b5a58be6a6c8a
Nathan Payre <nathan.payre@thetribe.io>
2022-01-31 14:09:58 +0100

    test

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,24 @@ import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
 bootstrapExtra().catch(e => console.error(e));
 
-let currentPopup: any = undefined;
-const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes();
+const handleTextPopup = (layerName: string, popupName: string, text: string) => {
+    let currentPopup: any = undefined;
 
-WA.room.onEnterLayer('clockZone').subscribe(() => {
-    console.log('toto')
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
-})
+    WA.room.onEnterLayer(layerName).subscribe(() => {
+        currentPopup = WA.ui.openPopup(popupName, text, []);
+    });
 
-WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
+    WA.room.onLeaveLayer(layerName).subscribe(() =>{
+        if (currentPopup !== undefined) {
+            currentPopup.close();
+            currentPopup = undefined;
+        }
+    });
+};
 
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
+handleTextPopup("zones/conferenceRoomPopupNameZone", "conferenceRoomPopupName", "Salle de conference (nom à déterminer)");
+handleTextPopup("zones/meetingRoom1PopupNameZone", "meetingRoom1PopupName", "Salle de réunions 1  (nom à déterminer)");
+handleTextPopup("zones/meetingRoom2PopupNameZone", "meetingRoom2PopupName", "Salle de réunions 2  (nom à déterminer)");
+handleTextPopup("zones/meetingRoom3PopupNameZone", "meetingRoom3PopupName", "Salle de réunions 3  (nom à déterminer)");
+
+WA.player.setOutlineColor(7, 55, 208);
